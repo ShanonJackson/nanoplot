58b108e18ddd65421635e13b44727261f1b3faf3
Kate <katshe18@gmail.com>
2024-12-02 15:11:29 +1300

    Setup Cypress e2e within PieGraph component folder

--- cypress/e2e/pie-graph.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('PieGraph Component', () => {
-  it('renders the pie chart', () => {
-      cy.visit('/pie-graph');
-      cy.get('.PieGraph_base__b9Chc').should('be.visible'); // Check for visibility
-  });
-});
