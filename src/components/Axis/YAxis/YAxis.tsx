8656479617d20277f386dc1044805309fa728126
Shanon Jackson <shanon.jackson@parrotanalytics.com>
2024-12-05 20:17:10 +1300

    polish left side nav

--- src/components/Axis/YAxis/YAxis.tsx
+++ src/components/Axis/YAxis/YAxis.tsx
@@ -9,21 +9,23 @@ type Props = {
 };
 
 export const YAxis = ({ context }: Props) => {
-	return <Graph.Column className={styles.base} style={{ border: "1px solid pink" }}>
-		{context?.domain.y.map((dp, i) => {
-			const top = 100 - MathUtils.scale(dp.coordinate, 3000, 100)
-			return (
-				<React.Fragment key={i}>
-					<div className={styles.tick} style={{top: `${top}%`}}>
-						{typeof dp.tick === "number" ? dp.tick.toFixed(2) : dp.tick.toString()}
-					</div>
-					<div className={styles.tickOpacity} style={{top: `${top}%`}}>
-						{typeof dp.tick === "number" ? dp.tick.toFixed(2) : dp.tick.toString()}
-					</div>
-				</React.Fragment>
-			)
-		})}
-	</Graph.Column>;
+	return (
+		<Graph.Column className={"relative"} style={{ border: "1px solid pink" }}>
+			{context?.domain.y.map((dp, i) => {
+				const top = 100 - MathUtils.scale(dp.coordinate, 3000, 100);
+				return (
+					<React.Fragment key={i}>
+						<div className={styles.tick} style={{ top: `${top}%` }}>
+							{typeof dp.tick === "number" ? dp.tick.toFixed(2) : dp.tick.toString()}
+						</div>
+						<div className={styles.tickOpacity} style={{ top: `${top}%` }}>
+							{typeof dp.tick === "number" ? dp.tick.toFixed(2) : dp.tick.toString()}
+						</div>
+					</React.Fragment>
+				);
+			})}
+		</Graph.Column>
+	);
 };
 
 YAxis.context = (ctx: GraphContext, props: Props) => {
