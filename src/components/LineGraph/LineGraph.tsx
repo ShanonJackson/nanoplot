8391eb16697631f40b92514acadc90160db58290
Shanon Jackson <shanon.jackson@parrotanalytics.com>
2024-12-21 09:15:56 +1300

    Line graph ready

--- src/components/LineGraph/LineGraph.tsx
+++ src/components/LineGraph/LineGraph.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import { GraphContext, XYDataset } from "@/hooks/use-graph";
 import { CoordinatesUtils } from "@/utils/coordinates/coordinates";
 import { GraphUtils } from "@/utils/graph/graph";
+import { ColorUtils } from "@/utils/color/color";
 
 type Props = {
 	children?: ReactNode;
@@ -9,40 +10,29 @@ type Props = {
 };
 
 export const LineGraph = ({ children, context }: Props) => {
-	console.log(context?.data);
-	if(!context) return null;
-	if(!GraphUtils.isXYData(context.data)) return null;
+	if (!context) return null;
+	if (!GraphUtils.isXYData(context.data)) return null;
 	const xForValue = CoordinatesUtils.xCoordinateFor(context);
 	const yForValue = CoordinatesUtils.yCoordinateFor(context);
-	const lines = context.data.map((line) => {
+	const lines = context.data.map((line, i, lines) => {
 		return {
 			...line,
-			data: line.data.map((xy) => {
-				return {
-					x: xForValue(xy.x),
-					y: yForValue(xy.y)
-				}
-			}),
+			id: line.id ?? line.name,
+			stroke: line.stroke ?? ColorUtils.colorFor(i, lines.length),
+			fill: line.fill === true ? (line.stroke ?? ColorUtils.colorFor(i, lines.length)) : line.fill,
+			data: line.data.map((xy) => ({
+				x: xForValue(xy.x),
+				y: yForValue(xy.y),
+			})),
 		};
-	})
+	});
 	return (
-		<svg viewBox={"0 0 3000 3000"} height={"100%"} width={"100%"} preserveAspectRatio={"none"}>
+		<svg viewBox={`0 0 ${context.viewbox.x} ${context.viewbox.y}`} height={"100%"} width={"100%"} preserveAspectRatio={"none"}>
 			{lines.map((line, i) => {
-				const d = line.data.map((xy, index) => {
-					if (index === 0) {
-						return "M " + xy.x + " " + xy.y
-					} else {
-						return "L " + xy.x + " " + xy.y
-					}
-				}).join(" ");
-				return <path 
-					key={i}
-					d={d}
-					fill={"transparent"}
-					stroke={"black"}
-					vectorEffect={"non-scaling-stroke"}
-					strokeWidth={1.5}
-				/>
+				const d = line.data.map((xy, index) => `${index === 0 ? "M" : "L"} ${xy.x} ${xy.y}`).join(" ");
+				return (
+					<path key={i} d={d} fill={"transparent"} stroke={line.stroke} vectorEffect={"non-scaling-stroke"} strokeWidth={1.5} />
+				);
 			})}
 			{children}
 		</svg>
