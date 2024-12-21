e0e029fe5f051a4ddd3ba6876745ef2a9c288e5c
Shanon Jackson <shanon.jackson@parrotanalytics.com>
2024-12-21 17:57:07 +1300

    remove dependency

--- build.tsx
+++ build.tsx
@@ -1,21 +1,30 @@
+import autoprefixer from "autoprefixer";
+import cssLoader from "bun-css-loader";
+import tailwindcss from "tailwindcss";
+
 (async () => {
 	await Bun.build({
 		entrypoints: [
-			'./src/components/PieGraph/PieGraph',
-			'./src/components/ScatterGraph/ScatterGraph',
-			'./src/components/BarGraph/BarGraph',
-			'./src/components/LineGraph/LineGraph',
-			'./src/components/Worldmap/Worldmap',
-			'./src/components/Legend/Legend',
+			"./src/components/PieGraph/PieGraph",
+			"./src/components/ScatterGraph/ScatterGraph",
+			"./src/components/BarGraph/BarGraph",
+			"./src/components/LineGraph/LineGraph",
+			"./src/components/Worldmap/Worldmap",
+			"./src/components/Legend/Legend",
 		],
-		outdir: './dist',
+		outdir: "./dist",
 		external: ["react", "react-dom"],
 		minify: true,
 		splitting: true,
 		naming: {
-			entry: '[dir]/[name].[ext]',
-			chunk: '[name]-[hash].[ext]',
-			asset: '[name]-[hash].[ext]',
+			entry: "[dir]/[name].[ext]",
+			chunk: "[name]-[hash].[ext]",
+			asset: "[name]-[hash].[ext]",
 		},
-	})
+		plugins: [
+			cssLoader({
+				postCssPlugins: [tailwindcss(), autoprefixer()],
+			}),
+		],
+	});
 })();
