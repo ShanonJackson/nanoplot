0c683eb7c8e16be9fb185ff5f836ecbed7f2c096
elassari19 <elassari19@gmail.com>
2025-01-06 20:45:58 +0100

    refactor and clean app pages

--- src/app/bar-graph/page.tsx
+++ src/app/bar-graph/page.tsx
@@ -1,69 +1,34 @@
 "use client";
-import { Graph } from "@/components/Graph/Graph";
-import { XAxis } from "@/components/XAxis/XAxis";
-import { YAxis } from "@/components/YAxis/YAxis";
-import { GridLines } from "@/components/GridLines/GridLines";
 import { Bars } from "@/components/Bars/Bars";
-import { ComponentProps, useState } from "react";
+import { useState } from "react";
 import { Control } from "@/components/Docs/Control/Control";
 import { BooleanControl } from "@/components/Docs/Control/components/BooleanControl/BooleanControl";
+import NanoPage from "@/components/Template/NanoPage";
+import { ControlGroup } from "../../components/ControlGroup/ControlGroup";
 
 export default function Page() {
-	const [gridline, setGridline] = useState<ComponentProps<typeof GridLines>>({});
 	const [bars, setBars] = useState<boolean>(false);
-	const setGridPartial = (partial: Partial<ComponentProps<typeof GridLines>>) => setGridline((prev) => ({ ...prev, ...partial }));
 
 	return (
-		<div className={"h-full max-h-screen grid grid-cols-[40%_1fr] grid-rows-2 gap-4"}>
-			<div className={"row-span-2 h-full border-[1px] border-dotted border-black dark:border-white p-4 bg-gray-100 dark:bg-gray-800"}>
-				<h1 className={"text-2xl"}>Bar Graph</h1>
-				<Control name={"Border"} type={"boolean"}>
-					<BooleanControl
-						value={gridline.border}
-						onChange={(checked) => setGridPartial({ border: checked })}
-						description={"Adds Border To Graph"}
-					/>
-				</Control>
-				<Control name={"Horizontal Grid"} type={"boolean"}>
-					<BooleanControl
-						value={gridline.horizontal}
-						onChange={(checked) => setGridPartial({ horizontal: checked })}
-						description={"Adds Horizontal Grid Lines"}
-					/>
-				</Control>
-				<Control name={"Vertical Grid"} type={"boolean"}>
-					<BooleanControl
-						value={gridline.vertical}
-						onChange={(checked) => setGridPartial({ vertical: checked })}
-						description={"Adds Vertical Grid Lines"}
-					/>
-				</Control>
+		<NanoPage
+			title={"Bar Graph"}
+			data={[
+				{
+					name: "My First Dataset",
+					data: MOCK_DATA.map(({ x, y }) => {
+						if (bars) return { x: y, y: x };
+						return { x, y };
+					}),
+				},
+			]}
+			graph={<Bars horizontal={bars} />}
+		>
+			<ControlGroup title="Base">
 				<Control name={"Horizontal Bras"} type={"boolean"}>
 					<BooleanControl value={bars} onChange={() => setBars(!bars)} description={"Display Bras horizontally"} />
 				</Control>
-			</div>
-
-			<div className={"h-full border-dotted border border-black dark:border-white"}>
-				<Graph
-					data={[
-						{
-							name: "My First Dataset",
-							data: MOCK_DATA.map(({ x, y }) => {
-								if (bars) return { x: y, y: x };
-								return { x, y };
-							}),
-						},
-					]}
-					gap={{ top: 15, left: 15, right: 30, bottom: 15 }}
-				>
-					<YAxis />
-					<GridLines {...gridline} />
-					<Bars horizontal={bars} />
-					<XAxis ticks={{ from: 0 }} />
-				</Graph>
-			</div>
-			<div className={"border-[1px] border-dotted border-black dark:border-white"}>EXAMPLES</div>
-		</div>
+			</ControlGroup>
+		</NanoPage>
 	);
 }
 
--- src/app/line-graph/page.tsx
+++ src/app/line-graph/page.tsx
@@ -1,130 +1,32 @@
 "use client";
-import { Graph } from "@/components/Graph/Graph";
-import { ComponentProps, useState } from "react";
-import { XAxis } from "@/components/XAxis/XAxis";
-import { YAxis } from "@/components/YAxis/YAxis";
 import { Lines } from "@/components/Lines/Lines";
-import { Legend } from "@/components/Legend/Legend";
-import { ControlGroup } from "@/components/ControlGroup/ControlGroup";
-import { BooleanControl } from "@/components/Docs/Control/components/BooleanControl/BooleanControl";
-import { GridLines } from "@/components/GridLines/GridLines";
-import { Control } from "@/components/Docs/Control/Control";
-import { HTMLControl } from "@/components/Docs/Control/components/HTMLControl/HTMLControl";
+import NanoPage from "@/components/Template/NanoPage";
 
 export default function Page() {
-	const [line, setLine] = useState<ComponentProps<typeof Lines>>({});
-	const [gridline, setGridline] = useState<ComponentProps<typeof GridLines>>({});
-	const [xaxis, setXAxis] = useState<ComponentProps<typeof XAxis>>({});
-	const [yaxis, setYAxis] = useState<ComponentProps<typeof YAxis>>({});
-	const [legend, setLegend] = useState<ComponentProps<typeof Legend>>({});
-
-	const setXAxisPartial = (partial: Partial<ComponentProps<typeof XAxis>>) => setXAxis((prev) => ({ ...prev, ...partial }));
-	const setYAxisPartial = (partial: Partial<ComponentProps<typeof YAxis>>) => setYAxis((prev) => ({ ...prev, ...partial }));
-	const setLegendPartial = (partial: Partial<ComponentProps<typeof Legend>>) => setLegend((prev) => ({ ...prev, ...partial }));
-	const setGridPartial = (partial: Partial<ComponentProps<typeof GridLines>>) => setGridline((prev) => ({ ...prev, ...partial }));
-	const setLinePartial = (partial: Partial<ComponentProps<typeof Lines>>) => setLine((prev) => ({ ...prev, ...partial }));
-
-	return (
-		<div className={"h-full max-h-screen grid grid-cols-[40%_1fr] grid-rows-2 gap-4"}>
-			<div className={"row-span-2 h-full border-[1px] border-dotted border-white"}>
-				<ControlGroup title={"Legend"}>
-					<Control name={"position"} type={"'top' | 'bottom' | 'left' | 'right'"}>
-						control pending...
-					</Control>
-					<Control name={"alignment"} type={"'center' | 'left' | 'right' | 'top'"}>
-						control pending...
-					</Control>
-				</ControlGroup>
-				<ControlGroup title={"YAxis"}>
-					<Control name={"title"} type={"ReactNode"}>
-						<HTMLControl html={yaxis.title?.toString() ?? ""} onChange={(html) => setYAxisPartial({ title: html })} />
-					</Control>
-					<Control name={"description"} type={"ReactNode"}>
-						<HTMLControl
-							html={yaxis.description?.toString() ?? ""}
-							onChange={(html) => setYAxisPartial({ description: html })}
-						/>
-					</Control>
-				</ControlGroup>
-				<ControlGroup title={"GridLines"}>
-					<Control name={"border"} type={"boolean"} default={"false"}>
-						<BooleanControl
-							value={gridline.border}
-							onChange={(checked) => setGridPartial({ border: checked })}
-							description={"Adds Border To Graph"}
-						/>
-					</Control>
-					<Control name={"horizontal"} type={"boolean"} default={"false"}>
-						<BooleanControl
-							value={gridline.horizontal}
-							onChange={(checked) => setGridPartial({ horizontal: checked })}
-							description={"Adds horizontal grid lines to graph"}
-						/>
-					</Control>
-					<Control name={"vertical"} type={"boolean"} default={"false"}>
-						<BooleanControl
-							value={gridline.vertical}
-							onChange={(checked) => setGridPartial({ vertical: checked })}
-							description={"Adds vertical grid lines to graph"}
-						/>
-					</Control>
-				</ControlGroup>
-				<ControlGroup title={"XAxis"}>
-					<Control name={"title"} type={"ReactNode"}>
-						<HTMLControl html={xaxis.title?.toString() ?? ""} onChange={(html) => setXAxisPartial({ title: html })} />
-					</Control>
-					<Control name={"description"} type={"ReactNode"}>
-						<HTMLControl
-							html={xaxis.description?.toString() ?? ""}
-							onChange={(html) => setXAxisPartial({ description: html })}
-						/>
-					</Control>
-				</ControlGroup>
-			</div>
-			<div className={"border-[1px] h-full border-dotted border-white"}>
-				<Graph
-					data={[
-						{
-							name: "Josh - Hours gamed",
-							data: [
-								{ x: 1, y: 20 },
-								{ x: 2, y: 40 },
-								{ x: 3, y: 30 },
-								{ x: 4, y: 50 },
-								{ x: 5, y: 36 },
-								{ x: 6, y: 60 },
-							],
-						},
-						{
-							name: "Sally - Hours gamed",
-							data: [
-								{ x: 1, y: 5.25 },
-								{ x: 2, y: 10 },
-								{ x: 3, y: 25.4 },
-								{ x: 4, y: 36 },
-								{ x: 5, y: 40 },
-								{ x: 6, y: 35 },
-							],
-						},
-					]}
-					gap={{ top: 15, left: 15, right: 36, bottom: 15 }}
-				>
-					<Legend position={"top"} alignment={"center"} />
-					<YAxis
-						{...yaxis}
-						title={<div dangerouslySetInnerHTML={{ __html: yaxis.title?.toString() ?? "" }} />}
-						description={<div dangerouslySetInnerHTML={{ __html: yaxis.description?.toString() ?? "" }} />}
-					/>
-					<GridLines {...gridline} />
-					<Lines />
-					<XAxis
-						{...xaxis}
-						title={<div dangerouslySetInnerHTML={{ __html: xaxis.title?.toString() ?? "" }} />}
-						description={<div dangerouslySetInnerHTML={{ __html: xaxis.description?.toString() ?? "" }} />}
-					/>
-				</Graph>
-			</div>
-			<div className={"border-[1px] border-dotted border-white"}>EXAMPLES</div>
-		</div>
-	);
+	return <NanoPage title={"Line Graph"} data={data} graph={<Lines />} />;
 }
+
+const data = [
+	{
+		name: "Josh - Hours gamed",
+		data: [
+			{ x: 1, y: 20 },
+			{ x: 2, y: 40 },
+			{ x: 3, y: 30 },
+			{ x: 4, y: 50 },
+			{ x: 5, y: 36 },
+			{ x: 6, y: 60 },
+		],
+	},
+	{
+		name: "Sally - Hours gamed",
+		data: [
+			{ x: 1, y: 5.25 },
+			{ x: 2, y: 10 },
+			{ x: 3, y: 25.4 },
+			{ x: 4, y: 36 },
+			{ x: 5, y: 40 },
+			{ x: 6, y: 35 },
+		],
+	},
+];
